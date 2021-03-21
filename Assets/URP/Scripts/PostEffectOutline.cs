ce0a7c1f5c82ccba37818c1e3ed6d4c8a6ef8506
adamwu <calvinmankor@gmail.com>
2021-03-21 21:54:58 +0800

    *：outline

---
+++ Assets/URP/Scripts/PostEffectOutline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PostEffectOutline : ScriptableRendererFeature
+{
+
+
+    public override void Create()
+    {
+
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+
+    }
+}
